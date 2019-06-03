e8985fbe225c22a8784db515a53b3adc70c16721
pfgallagher <pfgallagher@gmail.com>
2019-06-03 09:15:13 -0500

    Complete easy-040

---
+++ easy-040.ts
@@ -0,0 +1,9 @@
+const printOneToOneThousand = (num: number): boolean => {
+	console.log(num);
+	num++;
+	return num !== 1001 && printOneToOneThousand(num); // I'm not sure if short
+	// circuiting can technically be considered a conditional statement, but I
+	// can't think of any other straightforward way of doing this otherwise!
+};
+
+printOneToOneThousand(1);

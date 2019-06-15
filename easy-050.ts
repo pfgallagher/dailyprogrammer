b284b2531bebfeeb75118a393651b9aad7d984ee
pfgallagher <pfgallagher@gmail.com>
2019-06-15 13:02:29 -0500

    Complete easy-050

---
+++ easy-050.ts
@@ -0,0 +1,131 @@
+const storeCredit = (storeItems: number[], credits: number): number[] => {
+	const items: Array<{ price: number; complement: number }> = storeItems.map(
+		itemPrice => ({
+			complement: credits - itemPrice,
+			price: itemPrice,
+		}),
+	);
+	for (const item in items) {
+		if (items.hasOwnProperty(item)) {
+			const { complement, price } = items[item];
+			const complementIdx = storeItems.indexOf(complement);
+			if (complementIdx >= 0) {
+				const priceIdx = storeItems.indexOf(price);
+				return priceIdx < complementIdx
+					? [priceIdx, complementIdx]
+					: [complementIdx, priceIdx];
+			}
+		}
+	}
+	return [-1, -1];
+};
+
+console.log(storeCredit([5, 75, 25], 100));
+console.log(storeCredit([150, 24, 79, 50, 88, 345, 3], 200));
+console.log(
+	storeCredit(
+		[
+			230,
+			863,
+			916,
+			585,
+			981,
+			404,
+			316,
+			785,
+			88,
+			12,
+			70,
+			435,
+			384,
+			778,
+			887,
+			755,
+			740,
+			337,
+			86,
+			92,
+			325,
+			422,
+			815,
+			650,
+			920,
+			125,
+			277,
+			336,
+			221,
+			847,
+			168,
+			23,
+			677,
+			61,
+			400,
+			136,
+			874,
+			363,
+			394,
+			199,
+			863,
+			997,
+			794,
+			587,
+			124,
+			321,
+			212,
+			957,
+			764,
+			173,
+			314,
+			422,
+			927,
+			783,
+			930,
+			282,
+			306,
+			506,
+			44,
+			926,
+			691,
+			568,
+			68,
+			730,
+			933,
+			737,
+			531,
+			180,
+			414,
+			751,
+			28,
+			546,
+			60,
+			371,
+			493,
+			370,
+			527,
+			387,
+			43,
+			541,
+			13,
+			457,
+			328,
+			227,
+			652,
+			365,
+			430,
+			803,
+			59,
+			858,
+			538,
+			427,
+			583,
+			368,
+			375,
+			173,
+			809,
+			896,
+			370,
+			789,
+		],
+		542,
+	),
+);

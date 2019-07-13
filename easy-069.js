805ef13d967b0398acb7e833469c6209760057d6
pfgallagher <pfgallagher@gmail.com>
2019-07-12 22:40:38 -0500

    Complete easy-069 (bonus)

--- easy-069.js
+++ easy-069.js
@@ -1,3 +1,6 @@
+// Not gonna lie, this is pretty hacky.
+// It would definitely benefit from some refactoring, but I try to timebox these
+// challenges pretty tightly.
 var __read = (this && this.__read) || function (o, n) {
     var m = typeof Symbol === "function" && o[Symbol.iterator];
     if (!m) return o;
@@ -20,19 +23,50 @@ var __spread = (this && this.__spread) || function () {
 };
 var createColumn = function (title, inputs) {
     var longestColumnLength = Math.max.apply(Math, __spread(inputs.map(function (input) { return input.length; }), [title.length]));
-    var titleDifference = (longestColumnLength - title.length) / 2;
-    var formattedTitle = "| " + " ".repeat(titleDifference) + title + " ".repeat(titleDifference) + " |";
+    var titleDifference = Math.floor((longestColumnLength - title.length) / 2);
+    var parityOffset = titleDifference % 2 ? 1 : 2;
+    var formattedTitle = "|" + " ".repeat(titleDifference) + title + " " + " ".repeat(titleDifference + parityOffset) + "|";
     var formattedInputs = inputs
         .map(function (input) { return "| " + input.padEnd(longestColumnLength, " ") + " |"; })
         .reduce(function (acc, cur) { return acc + "\n" + cur; });
     var divider = "+" + "-".repeat(longestColumnLength + 2) + "+";
     return divider + "\n" + formattedTitle + "\n" + divider + "\n" + formattedInputs + "\n" + divider;
 };
-console.log(createColumn("Necessities", [
+var createTable = function (titles, inputs) {
+    if (typeof titles === "string") {
+        return createColumn(titles, inputs);
+    }
+    var columns = [];
+    titles.forEach(function (title, i) {
+        columns.push(createColumn(title, inputs[i]).split("\n"));
+    });
+    var arr = Array(columns[0].length).fill([]);
+    for (var i = 0; i < columns.length; i++) {
+        for (var j = 0; j < arr.length; j++) {
+            if (i > 0) {
+                arr[j] = arr[j].concat(columns[i][j].slice(1));
+            }
+            else {
+                arr[j] = arr[j].concat(columns[i][j]);
+            }
+        }
+    }
+    return arr.map(function (el) { return el.flat().join(""); }).join("\n");
+};
+console.log(createTable("Necessities", [
     "fairy",
     "cakes",
     "happy",
     "fish",
     "disgustipated",
     "melon-balls",
+]));
+console.log(createTable(["Name", "Address", "Description"], [
+    ["Reddit", "Wikipedia", "xkcd"],
+    ["www.reddit.com", "en.wikipedia.net", "xkcd.com"],
+    [
+        "The frontpage of the internet",
+        "The Free Encyclopedia",
+        "Sudo make me a sandwich.",
+    ],
 ]));
--- easy-069.ts
+++ easy-069.ts
@@ -1,13 +1,18 @@
-const createColumn = (title: string, inputs: string[]): string => {
+// Not gonna lie, this is pretty hacky.
+// It would definitely benefit from some refactoring, but I try to timebox these
+// challenges pretty tightly.
+
+const createColumn = (title: string, inputs: string[]) => {
 	const longestColumnLength = Math.max(
-		...inputs.map(input => input.length),
+		...(inputs as string[]).map(input => input.length),
 		title.length,
 	);
-	const titleDifference = (longestColumnLength - title.length) / 2;
-	const formattedTitle = `| ${" ".repeat(titleDifference)}${title}${" ".repeat(
-		titleDifference,
-	)} |`;
-	const formattedInputs = inputs
+	const titleDifference = Math.floor((longestColumnLength - title.length) / 2);
+	const parityOffset = titleDifference % 2 ? 1 : 2;
+	const formattedTitle = `|${" ".repeat(titleDifference)}${title} ${" ".repeat(
+		titleDifference + parityOffset,
+	)}|`;
+	const formattedInputs = (inputs as string[])
 		.map(input => `| ${input.padEnd(longestColumnLength, " ")} |`)
 		.reduce((acc, cur) => `${acc}\n${cur}`);
 	const divider = `+${"-".repeat(longestColumnLength + 2)}+`;
@@ -18,8 +23,32 @@ ${formattedInputs}
 ${divider}`;
 };
 
+const createTable = (
+	titles: string | string[],
+	inputs: string[] | string[][],
+): string => {
+	if (typeof titles === "string") {
+		return createColumn(titles, inputs as string[]);
+	}
+	const columns: any[] = [];
+	titles.forEach((title, i) => {
+		columns.push(createColumn(title, inputs[i] as string[]).split("\n"));
+	});
+	const arr = Array(columns[0].length).fill([]);
+	for (let i = 0; i < columns.length; i++) {
+		for (let j = 0; j < arr.length; j++) {
+			if (i > 0) {
+				arr[j] = arr[j].concat(columns[i][j].slice(1));
+			} else {
+				arr[j] = arr[j].concat(columns[i][j]);
+			}
+		}
+	}
+	return arr.map(el => el.flat().join("")).join("\n");
+};
+
 console.log(
-	createColumn("Necessities", [
+	createTable("Necessities", [
 		"fairy",
 		"cakes",
 		"happy",
@@ -27,4 +56,19 @@ console.log(
 		"disgustipated",
 		"melon-balls",
 	]),
+);
+
+console.log(
+	createTable(
+		["Name", "Address", "Description"],
+		[
+			["Reddit", "Wikipedia", "xkcd"],
+			["www.reddit.com", "en.wikipedia.net", "xkcd.com"],
+			[
+				"The frontpage of the internet",
+				"The Free Encyclopedia",
+				"Sudo make me a sandwich.",
+			],
+		],
+	),
 );

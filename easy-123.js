534ba8ad838275d481992158a1dc167c67039ab3
pfgallagher <pfgallagher@gmail.com>
2019-09-27 08:09:22 -0500

    Complete easy-123

---
+++ easy-123.js
@@ -0,0 +1,8 @@
+var digitalRoot = function (n) {
+    return n < 10 ? n : digitalRoot(Math.floor((n % 10) + n / 10));
+};
+console.log(digitalRoot(0)); // 0
+console.log(digitalRoot(1)); // 1
+console.log(digitalRoot(789)); // 6
+console.log(digitalRoot(31337)); // 8
+console.log(digitalRoot(1073741824)); // 1

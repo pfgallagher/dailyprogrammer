b1302ec42e2490577c2bd3fe2dd4ddc6c1e4a673
pfgallagher <pfgallagher@gmail.com>
2019-10-30 07:45:07 -0500

    Complete easy-146

---
+++ easy-146.ts
@@ -0,0 +1,4 @@
+const calcPolygonPerimeter = (n: number, r: number): string => (2 * r * n * Math.sin(Math.PI / n)).toFixed(3);
+
+console.log(calcPolygonPerimeter(5, 3.7)); // 21.748
+console.log(calcPolygonPerimeter(100, 1.0)); // 6.282

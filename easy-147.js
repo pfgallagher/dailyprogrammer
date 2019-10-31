2b31d9daa9797e252ac2254d52d49e6e1cb7e69b
pfgallagher <pfgallagher@gmail.com>
2019-10-31 08:16:38 -0500

    Complete easy-147

---
+++ easy-147.js
@@ -0,0 +1,7 @@
+// This is a linear solution that relies upon figuring out that only the first few scores aren't possible.
+// A more involved solution might search for combinations that add up to the score being tested.
+var validateFootballScore = function (score) { return [1, 2, 4, 5].includes(score) ? "Invalid Score" : "Valid Score"; };
+console.log(validateFootballScore(35));
+// Valid Score
+console.log(validateFootballScore(2));
+// Invalid Score

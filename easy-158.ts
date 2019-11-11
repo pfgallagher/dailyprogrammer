b2df28e4e98cab907ff2c2c0e36cf5341d7c348c
pfgallagher <pfgallagher@gmail.com>
2019-11-11 08:07:14 -0600

    Complete easy-158

---
+++ easy-158.ts
@@ -0,0 +1,18 @@
+import { range } from "./lib/util";
+
+const findTornNums = () =>
+	range(1, 99)
+		.filter(num => {
+			const digits = [...`${num ** 2}`];
+			if (digits.length === 4) {
+				const [one, two, three, four] = digits;
+				const target = parseInt(`${one}${two}${three}${four}`, 10);
+				const left = parseInt(`${one}${two}`, 10);
+				const right = parseInt(`${three}${four}`, 10);
+				return (left + right) ** 2 === target;
+			}
+			return false;
+		})
+		.map(num => num ** 2);
+
+console.log(findTornNums());

ce1e94dc12c792933127704e6a47f4b477444426
pfgallagher <pfgallagher@gmail.com>
2019-11-15 21:35:32 -0600

    Complete easy-162

---
+++ easy-162.js
@@ -0,0 +1,78 @@
+"use strict";
+exports.__esModule = true;
+var capitalizationRules = function (word) {
+    if (word.length > 1) {
+        var exclamationI = word.indexOf("!");
+        var caretI = word.indexOf("^");
+        if (exclamationI >= 0) {
+            return word.toUpperCase().slice(0, exclamationI);
+        }
+        if (caretI >= 0) {
+            return "" + word[0].toUpperCase() + word.slice(1, caretI);
+        }
+    }
+    return word;
+};
+var decompress = function (dictionary, compressedText) {
+    var rules = compressedText
+        .replace(/(\d*)/g, function (match) {
+        if (match) {
+            var iFromNum = parseInt(match, 10);
+            return dictionary[iFromNum];
+        }
+        return match;
+    })
+        .split(" ")
+        .map(function (el) { return capitalizationRules(el); })
+        .filter(function (el) { return el !== "e" && el !== "E"; })
+        .reduce(function (acc, cur, i, arr) {
+        var isPunctuation = /[\.,?!;:]/g;
+        if (isPunctuation.test(cur)) {
+            acc[acc.length - 1] = "" + acc[acc.length - 1] + cur;
+            return acc;
+        }
+        acc.push(cur);
+        return acc;
+    }, [])
+        .join(" ")
+        .replace(/ R ?/gi, "\n")
+        .replace(/ - /g, "-");
+    return rules;
+};
+console.log(decompress(["is", "my", "hello", "name", "stan"], "2! ! R 1^ 3 0 4^ . E"));
+// HELLO!
+// My name is Stan.
+var seussDictionary = [
+    "i",
+    "do",
+    "house",
+    "with",
+    "mouse",
+    "in",
+    "not",
+    "like",
+    "them",
+    "ham",
+    "a",
+    "anywhere",
+    "green",
+    "eggs",
+    "and",
+    "here",
+    "or",
+    "there",
+    "sam",
+    "am",
+];
+console.log(decompress(seussDictionary, "0^ 1 6 7 8 5 10 2 . R"));
+// I do not like them in a house.
+console.log(decompress(seussDictionary, "0^ 1 6 7 8 3 10 4 . R"));
+// I do not like them with a mouse.
+console.log(decompress(seussDictionary, "0^ 1 6 7 8 15 16 17 . R"));
+// I do not like them here or there.
+console.log(decompress(seussDictionary, "0^ 1 6 7 8 11 . R"));
+// I do not like them anywhere.
+console.log(decompress(seussDictionary, "0^ 1 6 7 12 13 14 9 . R"));
+// I do not like green eggs and ham.
+console.log(decompress(seussDictionary, "0^ 1 6 7 8 , 18^ - 0^ - 19 . R E"));
+// I do not like them, Sam-I-am.
